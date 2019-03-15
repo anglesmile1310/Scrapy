81c6dacb60d121b31a4cdc2e064b5c8941ed4c63
=anglesmile1310 <=>
2019-03-14 22:36:24 -0400

    crawl data vietnamnet tim kiem va trinh dien thong tin

---
+++ vietnamnet/spiders/vietnamnet.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy import Request
+
+
+class JobsSpider(scrapy.Spider):
+    name = 'vietnamnet'
+    start_urls = []
+
+    def start_requests(self):
+        urlRelative = 'https://vietnamnet.vn/vn/oto-xe-may/kham-pha/showroom-ty-do-o-campuchia-khien-nha-giau-viet-nam-phat-them-'
+        count = 0
+        for page in range(501000, 513001):
+            count = count + 1
+            url = urlRelative + str(page)+'.html'
+            print(url)
+            print('page - ', count)
+            yield scrapy.Request(url, self.parse)
+
+
+    def parse(self, response):
+            title = response.xpath('//title/text()').get()
+            content = "".join(response.xpath("//div[@class='ArticleContent']/p/text()").getall())
+            record = {'title': title, 'content': content}
+            yield record
